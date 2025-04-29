d4c44098cf2e90afc0dd1099276699fc29903268
RaylaJohnson28 <raylajohnson1@gmail.com>
2025-04-29 19:18:53 -0400

    uploading

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.body.style.background = "linear-gradient(180deg, blue 0%, purple 50%, black 100%)";
+document.body.style.backgroundAttachment = "fixed"; 
+document.body.style.margin = "0";
+document.body.style.minHeight = "100%";
